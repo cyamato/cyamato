d4d8e0fd4e08e2e76b9ffd9d7ebf05e7c46e6462
Craig Yamato <craig.yamato2@gmail.com>
2025-04-08 15:51:13 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd ~
-sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 rm ~/.zshrc
 wget https://raw.githubusercontent.com/cyamato/cyamato/refs/heads/main/.zshrc
-source ~/.zshrc
+KEEP_ZSHRC=yes
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
