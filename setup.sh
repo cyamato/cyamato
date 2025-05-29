3121482baa022c0f2bd3b8ac4e26f5f8093e6188
Craig Yamato <craig.yamato2@gmail.com>
2025-05-29 12:04:28 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,5 @@ cd ~
 rm ~/.zshrc
 wget https://raw.githubusercontent.com/cyamato/cyamato/refs/heads/main/.zshrc
 KEEP_ZSHRC=yes sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+ssh-import-id-gh cyamato
